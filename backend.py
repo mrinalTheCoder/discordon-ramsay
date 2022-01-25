a91ede7a4329d2a95d4cd2bd8f937be7594ca8ae
Mrinal Jain <2mrinaljain@gmail.com>
2022-01-25 11:32:38 +0530

    added scraping

--- backend.py
+++ backend.py
@@ -1,5 +1,22 @@
-def search_recipe(search_str):
-	return [
-		{'name': "Albino Pasta", 'link': "https://www.allrecipes.com/recipe/23173/albino-pasta/", 'img': 'https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F71335.jpg'},
-		{'name': "Pasta Pascal", 'link': "https://www.allrecipes.com/recipe/22200/pasta-pascal/", 'img': 'https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F953625.jpg'}
-	]
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+async def get_elements_from_tag(tag):
+	d = {}
+	d['name'] = tag['title']
+	d['link'] = tag['href']
+	div = tag.findChildren("img", recursive=True)[0]
+	d['img'] = div['src'][len('https://imagesvc.meredithcorp.io/v3/mm/image?url='):]
+	return d
+
+async def search_recipe(a):
+	a.replace(" ", "+")
+	searchURL="https://www.allrecipes.com/search/results/?search="+a
+	session = HTMLSession()
+	response = session.get(searchURL)
+	response.html.render(timeout=30)
+	soup = BeautifulSoup(response.html.html, "html.parser")
+	Title = soup.find_all("a", {"class":"card__titleLink manual-link-behavior"})
+	x = Title[:10:2]
+	x = list(map(get_elements_from_tag, x))
+	return x
--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from backend import search_recipe
 TOKEN = 'OTI2MDEzNDcyNjk5OTczNjcy.Yc1fQA.6EX2T7g8DK3KmbstOEbsWEPgfJk'
 bot = commands.Bot(command_prefix='!')
 
-
 @bot.command()
 async def greet(ctx):
 	await ctx.channel.send('hello')
@@ -19,7 +18,10 @@ async def search(ctx, arg):
 		img = img.replace('%3A', ':')
 		img = img.replace('%2F', '/')
 		embed = discord.Embed(title=f"{i+1}. {recipes[i]['name']}", url=recipes[i]['link'], color=0xEA3A44)
-		embed.set_image(url=img)
+		embed.set_image(url=img) 
 		await ctx.channel.send(embed=embed)
+	
+	msg = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+	await ctx.channel.send(f'`Confirmed, recipe {msg.content}`')
 
 bot.run(TOKEN)
