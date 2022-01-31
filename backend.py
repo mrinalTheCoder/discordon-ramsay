5ace24db4273ff31685f493c3bf97c8a65b426bc
Mrinal Jain <2mrinaljain@gmail.com>
2022-01-31 12:22:25 +0530

    fixed scraping

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
 
-async def get_elements_from_tag(tag):
+def get_elements_from_tag(tag):
 	d = {}
 	d['name'] = tag['title']
 	d['link'] = tag['href']
@@ -9,14 +9,16 @@ async def get_elements_from_tag(tag):
 	d['img'] = div['src'][len('https://imagesvc.meredithcorp.io/v3/mm/image?url='):]
 	return d
 
-async def search_recipe(a):
+def search_recipe(a):
 	a.replace(" ", "+")
 	searchURL="https://www.allrecipes.com/search/results/?search="+a
-	session = HTMLSession()
-	response = session.get(searchURL)
-	response.html.render(timeout=30)
-	soup = BeautifulSoup(response.html.html, "html.parser")
-	Title = soup.find_all("a", {"class":"card__titleLink manual-link-behavior"})
+	response = requests.get(searchURL)
+	content = response.content
+	soup = BeautifulSoup(content, "html.parser")
+	#print(soup)
+	#Title = soup.find_all("a", {"class":"card__titleLink manual-link-behavior"})
+	Title = soup.find_all("a", {"class":"card__titleLink"})
+	#print(Title)
 	x = Title[:10:2]
 	x = list(map(get_elements_from_tag, x))
 	return x
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from backend import search_recipe
+import emoji
 
 TOKEN = 'OTI2MDEzNDcyNjk5OTczNjcy.Yc1fQA.6EX2T7g8DK3KmbstOEbsWEPgfJk'
 bot = commands.Bot(command_prefix='!')
@@ -9,6 +10,14 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def greet(ctx):
 	await ctx.channel.send('hello')
+
+@bot.command()
+async def cmds(ctx):
+	embed = discord.Embed(colour = discord.Colour.red())
+	embed.set_author(name = 'COMMAND LIST')
+	embed.add_field(name = '!search', value = 'place this keyword before you search for any recipe',inline=True)
+	embed.add_field(name = '!greet', value = 'for a greeting message',inline=False)
+	await ctx.channel.send( embed=embed)
 
 @bot.command()
 async def search(ctx, arg):
@@ -25,39 +34,3 @@ async def search(ctx, arg):
 	await ctx.channel.send(f'`Confirmed, recipe {msg.content}`')
 
 bot.run(TOKEN)
-'''import discord
-from discord.ext import commands
-import os
-from backend import search_recipe
-import emoji
-
-TOKEN = 'OTI2MDEzNDcyNjk5OTczNjcy.Yc1fQA.6EX2T7g8DK3KmbstOEbsWEPgfJk'
-bot = commands.Bot(command_prefix='!')
-
-@bot.command()
-async def greet(ctx):
-	await ctx.channel.send('hello')
-
-@bot.command()
-async def search(ctx, arg):
-	recipes = search_recipe(arg)
-	for i in range(min(len(recipes), 5)):
-		img = recipes[i]['img']
-		img = img.replace('%3A', ':')
-		img = img.replace('%2F', '/')
-		embed = discord.Embed(title=f"{i+1}. {recipes[i]['name']}", url=recipes[i]['link'], color=0xEA3A44)
-		embed.set_image(url=img) 
-		await ctx.channel.send(embed=embed)
-	
-	msg = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
-	await ctx.channel.send(f'`Confirmed, recipe {msg.content}`')
-@bot.command()
-async def cmds(ctx):
-    embed = discord.Embed(
-        colour = discord.Colour.red() )
-    embed.set_author(name = 'COMMAND LIST')
-    embed.add_field(name = '!search', value = 'place this keyword before you search for any recipe',inline=True)
-    embed.add_field(name = '!greet', value = 'for a greeting message',inline=False)
-    
-    await ctx.channel.send( embed=embed)
-bot.run(TOKEN)'''
